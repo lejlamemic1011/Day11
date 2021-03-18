ad0a9700f65176bedb7bcd2b9d75c9c5ea4efab2
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-18 22:43:06 +0100

    Update index.js

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,23 +1,22 @@
-
-// Only change code below this line
 function myBouncer(arr) {
-  var result = [];
-
-  for(var i = 0; i < arr.length; i++){
-      if(Boolean(arr[i]) === true){
-          result.push(arr[i]);
-      }
+  for (i = 0; i < arr.length; i++) {
+    if (
+      arr[i] === false ||
+      arr[i] === null ||
+      Number.isNaN(arr[i]) ||
+      arr[i] === 0 ||
+      arr[i] == "" ||
+      arr[i] === undefined
+    ) {
+      arr.splice(i, 1);
+      i--;
+    }
   }
-
-    return result;
+  return arr;
 }
-
-// Only change code above this line
-
 console.log(myBouncer([7, "ate", "", false, 9]));
 console.log(myBouncer(["a", "b", "c"]));
 console.log(myBouncer([false, null, 0, NaN, undefined, ""]));
 console.log(myBouncer([null, NaN, 1, 2, undefined]));
-
 
 module.exports = myBouncer;
