a9ea2e885ac3b551befb23b16e8d806ab4a2620f
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-18 22:41:47 +0100

    Update index.js

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,23 +1,15 @@
-
-// Only change code below this line
 function myMutation(arr) {
-    var firstStr = arr[0].toLowerCase();
-    var secondStr = arr[1].toLowerCase();
-    
-    for(var i = 0; i < secondStr.length; i++){
-        if(firstStr.indexOf(secondStr[i]) === -1){
-            return false;
-        } 
-    }
+  let secondValue = arr[1].toLowerCase();
+  let firstValue = arr[0].toLowerCase();
+  for (let i = 0; i < secondValue.length; i++) {
+    if (firstValue.indexOf(secondValue[i]) < 0) return false;
+  }
 
-    return true;
+  return true;
 }
-
-// Only change code above this line
-
 console.log(myMutation(["hello", "hey"]));
 console.log(myMutation(["hello", "Hello"]));
-console.log(myMutation(["zyxwvutrsqponmlkjihgfedcab", "qrstu"]));
+console.log(myMutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));
 console.log(myMutation(["Mary", "Army"]));
 console.log(myMutation(["Mary", "Aarmy"]));
 console.log(myMutation(["Alien", "line"]));
