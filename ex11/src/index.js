9faf609826693b95970103cd47fec67119616176
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-18 16:38:55 +0100

    Add files via upload

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,1 +1,12 @@
+// Only change code below this line
+function splitArrayInGroups(arr, n) {
+  result = [];
+  for (var i = 0; i < arr.length; i += n) {
+    result.push(arr.slice(i, i + n));
+  }
+  return result;
+}
+// Only change code above this line
 
+console.log(splitArrayInGroups(["a", "b", "c", "d"], 2));
+module.exports = splitArrayInGroups;
