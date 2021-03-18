4ce4b6c025200de79771a5fa36ab24776ff4b8ef
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-18 22:30:33 +0100

    Update index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,32 +1,29 @@
-// Create a temps array here
 var temps = [
-    [31, 32, 19, 37],
-    [29, 27, 55, 36],
-    [17, 27, 42, 46],
-    [29, 52, 21, 64],
-    [91, 27, 31, 61]
+  [31, 32, 19, 37],
+  [29, 27, 55, 36],
+  [17, 27, 42, 46],
+  [29, 52, 21, 64],
+  [91, 27, 31, 61],
 ];
-// End of temps array
 
 function myArrayFunction(arr) {
-    var newTemps = [...arr];
-    var averageDayTemp = [];
-    // Only change code below this line
-    var tempDaySum = 0;
-    var avgTemp;
-    for(var i = 0; i < newTemps.length; i++){
-        for(var j = 0; j < newTemps[i].length; j++){
-            tempDaySum += newTemps[i][j];
-        }
-        avgTemp = tempDaySum / newTemps[i].length;
-        averageDayTemp.push(avgTemp);
-        tempDaySum = 0;
-    }
-    
-    // Only change code above this line
-    return averageDayTemp;
-}
+  var newTemps = [];
+  var averageDayTemp = [];
 
+  for (i = 0; i < arr.length; i++) {
+    newTemps = arr[i];
+    var sum = 0;
+
+    for (k = 0; k < newTemps.length; k++) {
+      sum += newTemps[k];
+    }
+
+    var averageDayValue = sum / newTemps.length;
+    averageDayTemp.push(averageDayValue);
+  }
+  return averageDayTemp;
+}
+myArrayFunction(temps);
 console.log(myArrayFunction(temps));
 
 module.exports = myArrayFunction;
