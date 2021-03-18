a8a09edd22e4416d99fa26b1850a175673318842
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-18 22:38:45 +0100

    Update index.js

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,22 +1,32 @@
-
-// Only change code below this line
 function multiplyArrayFunction(myArray) {
-    var product = 1;
-    var sum = 0;
-    
-    for(var i = 0; i < myArray.length; i++){
-        for(var j = 0; j < myArray[i].length; j++){
-            sum += myArray[i][j];
-            product *= myArray[i][j];
-        }
+  var sum = 0;
+  var product = 1;
+
+  for (i = 0; i < myArray.length; i++) {
+    let arreyItem = myArray[i].length;
+
+    if (arreyItem > 1) {
+      let subArr = myArray[i];
+
+      for (k = 0; k < subArr.length; k++) {
+        sum += Number(subArr[k]);
+        product *= Number(subArr[k]);
+      }
+    } else {
+      sum += Number(myArray[i]);
+      product *= Number(myArray[i]);
     }
-
-    return [product, sum];
+  }
+  var arr = [product, sum];
+  return arr;
 }
-// Only change code above this line
-
 console.log(multiplyArrayFunction([[2], [5, 6, 7], [8, 9]]));
-console.log(multiplyArrayFunction([[2.5, 2], [0.5,0.2], [8]]));
-console.log(multiplyArrayFunction([[1,2], [3,4,5,6], [7,8,9]]));
-
+console.log(multiplyArrayFunction([[2.5, 2], [0.5, 0.2], [8]]));
+console.log(
+  multiplyArrayFunction([
+    [1, 2],
+    [3, 4, 5, 6],
+    [7, 8, 9],
+  ])
+);
 module.exports = multiplyArrayFunction;
