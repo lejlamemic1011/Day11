9245645ed7aa66389fd63ee09e01f068990df033
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-20 19:21:06 +0100

    Update index.js

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,14 +1,13 @@
+// Only change code below this line
 function mySplice(arr1, arr2, n) {
-  const newArray = arr2.slice();
-  const newArrayRevers = arr1.reverse();
-  newArray.splice(n, 0, ...newArrayRevers);
-
-  return newArray;
+    for (var i = 0; i < arr1.length; i++) {
+        arr2.splice(n, 0, arr1[i]);
+    }
+    return arr2;
 }
-
-console.log(mySplice([1, 2, 3], [4, 5], 1));
-console.log(mySplice([1, 2, 3], [4, 5], 2));
-console.log(mySplice(["b", "c"], ["a", "d"], 1));
-console.log(mySplice(["Dell", "Philips"], ["Samsung", "AOX", "Sony"], 2));
-
+// Only change code above this line
+console.log(mySplice([1, 2, 3], [4, 5], 1)); // Change this line
+console.log(mySplice([1, 2, 3], [4, 5], 2)); // Change this line
+console.log(mySplice(["b", "c"], ["a", "d"], 1)); // Change this line
+console.log(mySplice(["Dell", "Philips"], ["Samsung", "AOX", "Sony"], 2)); // Change this line
 module.exports = mySplice;
