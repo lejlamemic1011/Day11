8e83e9fe024fbadd0efc9aae38cac905f6568e80
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-20 19:22:12 +0100

    Update index.js

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,16 +1,16 @@
+// Only change code below this line
 function splitArrayInGroups(arr, n) {
-  result = [];
-  while (arr.length > n) {
-    result.push(arr.splice(0, n));
-  }
-  if (arr.length) result.push(arr);
-  return result;
-}
-
-console.log(splitArrayInGroups(["a", "b", "c", "d"], 2));
-console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
-console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2));
-console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3));
-console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
-
+    var result = [];
+    
+    for (var i = 0; i < n; i++) {
+        result.push(arr.splice(0, n))
+    }
+    return result;
+}    
+// Only change code above this line
+console.log(splitArrayInGroups(["a", "b", "c", "d"], 2)); // Change this line
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)); // Change this line
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2)); // Change this line
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3)); // Change this line
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)); // Change this line
 module.exports = splitArrayInGroups;
