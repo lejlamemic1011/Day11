1ad0e6c331e1d7d38ae71626158f31b61bf87a2a
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-20 19:12:39 +0100

    Update index.js

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,11 +1,10 @@
-var myNestedArray = [
-  ["Toblerone", 5],
-  ["Milka", 3],
-];
+// Only change code below this line
+var myNestedArray = [["Toblerone", 5], ["Milka", 3]];
 
 function myNestedFunction(arr) {
   return arr;
 }
 
+// Only change code above this line
 console.log(myNestedFunction(myNestedArray));
-module.exports = myNestedFunction;
+module.exports = myNestedArray;
