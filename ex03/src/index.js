63f66d2d76606c7171b666887e45f1bc91e523d9
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-20 19:10:27 +0100

    Update index.js

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,17 +1,17 @@
+// Create a myPetsArray array here
 var myPetsArray = ["Dog", "Cat"];
+// End of myPetsArray array
 
 function myArrayFunction(myPets) {
   var myNewPets = [...myPets];
 
+  // only change code below this line
   myNewPets.push("Bird", "Fish");
-  var firstPet = myNewPets[0];
-  var lastPet = myNewPets[myNewPets.length - 1];
-  myNewPets.shift();
-  myNewPets.unshift();
+  var firstPet = myNewPets.shift();
+  var lastPet = myNewPets.pop();
   myNewPets.unshift("Lion");
-  console.log(firstPet);
-  console.log(lastPet);
   return myNewPets;
+  // Only change code above this line
 }
 
 console.log(myArrayFunction(myPetsArray));
